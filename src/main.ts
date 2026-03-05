d9cfb8045e25e5be584f04249448a762fbedaa43
khian-23 <mecoycokhian2@gmail.com>
2026-03-05 09:49:20 +0800

    deploy

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,19 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: [
+      'https://espproject-bafd4.web.app',
+      'https://espproject-bafd4.firebaseapp.com',
+      'http://localhost:3000',
+      'http://localhost:5173',
+    ],
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    allowedHeaders: 'Content-Type,Authorization,x-device-key',
+  });
   app.setGlobalPrefix('api');
-  await app.listen(3000, '0.0.0.0');
-  
+
+  const port = Number(process.env.PORT) || 3000;
+  await app.listen(port, '0.0.0.0');
 }
 bootstrap();
